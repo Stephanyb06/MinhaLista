3cbe6c0030dd36c5bbb035563af3260be2d21a56
STEPHAN� BRAGA DOS SNTOS <stephany.3279@aluno.pr.senac.br>
2025-08-19 09:09:23 -0300

    codificando icons, baixando biblioteca dos icons e arrumando o input e a estilizacao

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,22 +1,38 @@
 import React from "react";
 import { Text, View, Image, TextInput } from "react-native";
 import { style } from "./styles";
-import Logo from "../../assets/logo1.png"
+import Logo from "../../assets/logo1.png";
+import {MaterialIcons} from '@expo/vector-icons'
+import { themas } from "../../global/themes"
 
 export default function Login() {
     return (
         <View style={style.container}>
             <View style={style.boxTop}> 
                 <Image source={Logo} style={style.logo} resizeMode="contain"/>
-                <Text style={style.text}>Bem vindo de volta</Text>
+                <Text style={style.text}>Bem vindo de volta!</Text>
             </View>
             <View style={style.boxMid} >
                 <Text style={style.titleInput}>Endereço de E-mail</Text>
                 <View style={style.boxInput}>
                     <TextInput style={style.input}/>
+                    <MaterialIcons 
+                    name = "email"
+                    size={20}
+                    color={themas.colors.gray}
+                    />
                 </View>
                 <Text style={style.titleInput}>Senha</Text>
-                <TextInput />
+                <View style={style.boxInput}>
+                    <TextInput
+                        style={style.input}
+                    />
+                    <MaterialIcons 
+                    name = "remove-red-eye"
+                    size={20}
+                    color={themas.colors.gray}
+                    />
+                </View>
             </View>
             <View style={style.boxBottom}>
             </View>
