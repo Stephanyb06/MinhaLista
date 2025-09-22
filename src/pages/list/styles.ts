4565bd9f1716e10c9028ee9a8b323a2faf921fb9
STEPHAN� BRAGA DOS SNTOS <stephany.3279@aluno.pr.senac.br>
2025-09-22 11:21:15 -0300

    Criando componente ball, inicializando seu index e style dentro de sua pasta, aplicando estilos para este componente

--- src/pages/list/styles.ts
+++ src/pages/list/styles.ts
@@ -44,6 +44,12 @@ export const style = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between'
+    },
+    rowCardLeft: {
+        width: '70%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10
     }
 
 })
