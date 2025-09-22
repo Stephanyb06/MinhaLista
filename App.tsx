7b74a8104813ce8cc34e7b04f87247c760c4ab52
STEPHAN� BRAGA DOS SNTOS <stephany.3279@aluno.pr.senac.br>
2025-09-22 16:48:16 -0300

    Aplicando modalize, criando tela de interacao no modal e mexendo no AuthContextList.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import './gesture-handler';
 import { StyleSheet } from 'react-native';
 import Routes from './src/routes/index.routes';
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
