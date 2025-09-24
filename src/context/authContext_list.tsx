91395c1a8c627e964f42a2fb5089f26319e7c946
STEPHAN� BRAGA DOS SNTOS <stephany.3279@aluno.pr.senac.br>
2025-09-24 08:25:32 -0300

    Colocando inputs no modal, estilizando eles com novos atributos dos componentes

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -1,7 +1,8 @@
-import React, { createContext, useContext, useRef } from "react";
+import React, { createContext, useContext, useRef, useEffect } from "react";
 import { Dimensions, Text, View, StyleSheet, Touchable, TouchableOpacity} from "react-native";
 import {MaterialIcons, AntDesign} from '@expo/vector-icons';
 import {Modalize} from "react-native-modalize";
+import { Input } from "../components/input";
 
 export const AuthContextList: any = createContext({});
 
@@ -11,6 +12,10 @@ export const AuthProviderList = (props: any): any => {
     const onOpen = () => {
         modalizeRef?.current?.open();
     }
+
+    useEffect(() => {
+        onOpen()
+    }, [])
     const _container = () => {
         return (
             <View style={styles.container}>
@@ -22,7 +27,7 @@ export const AuthProviderList = (props: any): any => {
                         />
                     </TouchableOpacity>
 
-                    <Text>Criar tarefa</Text>
+                    <Text style={styles.title}>Criar tarefa</Text>
 
                     <TouchableOpacity>
                         <AntDesign
@@ -30,6 +35,31 @@ export const AuthProviderList = (props: any): any => {
                             size={30}
                         />
                     </TouchableOpacity>
+                </View>
+                <View style={styles.content}>
+                    <Input 
+                    title="Título"
+                    labelStyle={styles.label}
+                    />
+                    <Input 
+                    title="Descrição"
+                    labelStyle={styles.label}
+                    height={100}
+                    multiline
+                    numberOfLines={5}
+                    />
+                </View>
+                <View style={{width:'40%'}}>
+                    <Input 
+                    title="Tempo Limite:"
+                    labelStyle={styles.label}
+                    />
+                </View>
+                <View style={styles.containerFlag}>
+                    <Text style={styles.label}>Flags:</Text>
+                    <View style={{}}>
+
+                    </View>
                 </View>
             </View>
         )
@@ -51,5 +81,32 @@ export const AuthProviderList = (props: any): any => {
 
 export const useAuth = () => useContext(AuthContextList);
 export const styles = StyleSheet.create({
-
+        container: {
+            width: '100%',
+        },
+        header: {
+            width: '100%',
+            height: 40,
+            paddingHorizontal: 40,
+            flexDirection: 'row',
+            marginTop: 20,
+            justifyContent: 'space-between',
+            alignContent: 'center'
+        },
+        title: {
+            fontSize: 20,
+            fontWeight: 'bold'
+        },
+        content: {
+            width: '100%',
+            paddingHorizontal: 20
+        },
+        containerFlag: {
+            width: '100%',
+            padding: 10
+        },
+        label: {
+            fontWeight: 'bold',
+            color: '#000'
+        }
 })
