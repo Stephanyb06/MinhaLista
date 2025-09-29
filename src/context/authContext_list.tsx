0ffd9cf596d7fd3520a66bf2eae0dd9923133252
STEPHAN� BRAGA DOS SNTOS <stephany.3279@aluno.pr.senac.br>
2025-09-29 16:57:06 -0300

    Baixando banco de dados para armazenar nossos arquivos JSON, colocando funcionalidade no modal para guardar dados no terminal para entendimento do App

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -6,6 +6,7 @@ import { Input } from "../components/input";
 import { themas } from "../global/themes";
 import { Flag } from "../components/Flag";
 import CustomDateTimePicker from "../components/CustomDateTimePicker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContextList: any = createContext({});
 
@@ -32,10 +33,6 @@ export const AuthProviderList = (props: any): any => {
         modalizeRef?.current?.close();
     }
 
-    useEffect(() => {
-        onOpen()
-    }, [])
-
     const _renderFlags = () => {
         return (
             flags.map((item, index) => (
@@ -55,6 +52,16 @@ export const AuthProviderList = (props: any): any => {
     const handleTimeChange = (date) => {
         setSelected(date);
     }
+    const handleSave = () => {
+        const newItem = {
+            item: 0,
+            title: 'Titulo',
+            description: 'Descrição',
+            flags: 'Flags',
+            timeLimite: '01.02.2025'
+        }
+        console.log(newItem)
+    }
     const _container = () => {
         return (
             <KeyboardAvoidingView
@@ -71,7 +78,7 @@ export const AuthProviderList = (props: any): any => {
 
                     <Text style={styles.title}>Criar tarefa</Text>
 
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleSave()}>
                         <AntDesign
                             name="check"
                             size={30}
