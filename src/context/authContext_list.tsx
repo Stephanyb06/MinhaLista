ac63b94623a37bb0e088e93bc90e4383fcaedf81
STEPHAN� BRAGA DOS SNTOS <stephany.3279@aluno.pr.senac.br>
2025-10-03 12:25:01 -0300

    Criando tasklist para salvar tarefa, criando constantes para ela e os sets quando salvar a tarefa

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -26,6 +26,7 @@ export const AuthProviderList = (props: any): any => {
     const [showDatePicker, setShowDatePicker] = useState(false);
     const [showTimePicker, setShowTimePicker] = useState(false);
     const [item, setItem] = useState(0);
+    const [taskList, setTaskList] = useState([]);
 
     const onOpen = () => {
         modalizeRef?.current?.open();
@@ -33,6 +34,10 @@ export const AuthProviderList = (props: any): any => {
     const onClose = () => {
         modalizeRef?.current?.close();
     }
+
+    useEffect(() => {
+        console.log(taskList.length)
+    },[taskList]);
 
     const _renderFlags = () => {
         return (
@@ -76,17 +81,28 @@ export const AuthProviderList = (props: any): any => {
                 ).toISOString(),
             }
 
-            const storageData = await AsyncStorage.getItem('tasklist');
-            console.log(storageData)
-            let tasklist = storageData ? JSON.parse(storageData) : [];
-            tasklist.push(newItem);
-            await AsyncStorage.setItem('tasklist', JSON.stringify(tasklist))
+            const storageData = await AsyncStorage.getItem('taskList');
+            // console.log(storageData)
+            let taskList = storageData ? JSON.parse(storageData) : [];
+            taskList.push(newItem);
+            await AsyncStorage.setItem('taskList', JSON.stringify(taskList))
+
+            setTaskList(taskList),
+            setData(),
+            onClose
 
         } catch (error) {
             console.log("Error ao salvar o item", error)
         }
     }
-
+    const setData = () => {
+        setTitle('')
+        setDescription(''),
+        setSelectedFlag('Urgente'),
+        setItem(0)
+        setSelectedDate(new Date())
+        setSelectedTime(new Date())
+    }
 
     const _container = () => {
         return (
@@ -177,7 +193,7 @@ export const AuthProviderList = (props: any): any => {
         )
     }
     return (
-        <AuthContextList.Provider value={{ onOpen }}>
+        <AuthContextList.Provider value={{ onOpen, taskList }}>
             {props.children}
             <Modalize
                 ref={modalizeRef}
